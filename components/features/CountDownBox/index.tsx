f0267ec1cd09cded3228b93a02effca612071f6d
cryptdev27 <153510373+cryptdev27@users.noreply.github.com>
2023-12-23 23:37:24 +0000

    change sale level calc to account for TOTAL_CTB_SUPPLY

--- components/features/CountDownBox/index.tsx
+++ components/features/CountDownBox/index.tsx
@@ -11,6 +11,7 @@ import styles from "./countdown-box.module.scss";
 import { useWalletStore } from "store/wallet";
 import toast from "services/toastService";
 import { getBalance, initLucid, lucid } from "utils/lucidUtils";
+import { TOTAL_CTB_SUPPLY } from "contracts/addresses";
 
 const PURCHASE_ADDRESS = "addr1q8cal4hqc6qxxcwvhddsyyl7kj8acf4dhh4xuykyk5xxkx475hue5kgfa44h57v80pclaqnshgzdv3kc9tldq3eza3gquuaxdl";
 
@@ -35,7 +36,7 @@ const getWalletBalance = async () => {
 
 const saleAmount = Math.round(await getSaleAmount());
 
-const saleLevel = (saleAmount / 2000000) * 100;
+const saleLevel = (saleAmount / TOTAL_CTB_SUPPLY) * 100;
 
 const walletBalance = Math.round(await getWalletBalance());
 
